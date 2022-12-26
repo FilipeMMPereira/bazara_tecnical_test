e7f6841aacfaa6246a539c3a12c62bb3b075f5da
FilipePereira997 <68111743+FilipePereira997@users.noreply.github.com>
2022-12-27 01:37:48 +0200

    size

--- lib/src/pages/auth/SignInScreen.dart
+++ lib/src/pages/auth/SignInScreen.dart
@@ -1,3 +1,4 @@
+
 import 'package:bazara_tecnical_test/src/provider/google_sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -14,29 +15,35 @@ class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.blue,
         body: Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('BAZARA TECNICAL TEST'),
-            ElevatedButton.icon(
-                icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    onPrimary: Colors.black,
-                    minimumSize: Size(double.infinity, 50)),
-                onPressed: () {
-                  final provider =
-                      Provider.of<GoogleSignInProvider>(context, listen: false);
-                  provider.googleLogin();
-                },
-                label: const Text('Sign Up with Google')),
-          ],
-        ),
-      ),
-    ));
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('BAZARA TECNICAL', style:TextStyle(color: Colors.red, fontSize: 30, fontWeight: FontWeight.bold),),
+                Text('TEST', style:TextStyle(color: Colors.red, fontSize: 30, fontWeight: FontWeight.bold),),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton.icon(
+                    icon: FaIcon(FontAwesomeIcons.google, color: Colors.red),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        onPrimary: Colors.black,
+                        minimumSize: Size(double.infinity, 50)),
+                    onPressed: () {
+                      final provider = Provider.of<GoogleSignInProvider>(
+                          context,
+                          listen: false);
+                      provider.googleLogin();
+                    },
+                    label: const Text('Sign Up with Google')),
+              ],
+            ),
+          ),
+        ));
   }
 }
